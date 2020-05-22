99fe36c610c60f01042e42e4a1d4d5f7eee2521a
mkochalko <matthew.kochalko@gmail.com>
2020-05-21 21:42:41 -0700

    Remove extra spaces and separate lines into lists

--- import.py
+++ import.py
@@ -14,8 +14,29 @@ def extract_information(pdf_path):
             lines = page.splitlines()
             for line in lines:
                 if line != '':
-                    print(line)
-                    # print('new Line')
+                    print(remove_extra_spaces(line))
+                    print('new Line')
+
+def remove_extra_spaces(line):
+    new_line = ''
+    for i in range(len(line)):
+        if line[i] != ' ':
+            new_line += line[i]
+        elif line[i] == ' ':
+            if i != 0 and line[i-1] != ' ':
+                new_line += ' '
+            else:
+                new_line += '|'
+
+    return convert_lines_to_list(new_line[1:])
+
+def convert_lines_to_list(line):
+    arr = line.split('|')
+    result = []
+    for item in arr:
+        if item:
+            result.append(item)
+    return result
             
 def write_to_spreadsheet(file):
     workbook = xlsxwriter.Workbook(file)
@@ -23,6 +44,13 @@ def write_to_spreadsheet(file):
     # worksheet.write('C5', 'test')
     worksheet.write('C7', 'it works')
     workbook.close()
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     path = 'march.pdf'
